e87a8f2032ce4550235832a740b332dec3821d11
Max <72516602+maxas10@users.noreply.github.com>
2025-07-01 04:48:22 -0700

    fix dating error

--- birthdays/index.js
+++ birthdays/index.js
@@ -1,105 +1,3 @@
-// const cards = document.getElementsByClassName("item");
-// const rows = document.getElementsByClassName("row");
-// const birthdaypeople = document.getElementById("birthdaypeople");
-// const people = document.getElementById("people");
-
-
-// var oldDates = [];
-// var newDates = [];
-// var evenNewerDates = [];
-// var newNames = [];
-// var date = new Date();
-// var enableSection = false;
-// var onlyonceagainagainagain = false;
-// var hasbeenSorted = false;
-// var cardString;
-
-// function checkTime() {
-//     if (date.getHours() >= 19 || date.getHours() <= 6) {
-//         document.body.style.backgroundColor = "#070620";
-//         document.body.style.color = "white";
-//         document.getElementById("container").style.backgroundColor = "#160f3a";
-//         colors = ["maroon", "darkorange", "goldenrod", "darkgreen", "darkblue", "purple"];
-//         onlyonceagainagain();
-//     }
-// }
-
-// function onlyonceagainagain() {
-//     if (onlyonceagainagainagain == false) {
-//         onlyonceagainagainagain = true;
-//         for (let i = 0; i < names.length; i++) {
-//             cards[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length) + 0];
-//         }
-//     }
-// }
-
-// for (let i = 0; i < names.length; i++) {
-//     oldDates.push(dates[i]);
-//     cards[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length) + 0];
-//     cards[i].innerHTML = names[i] + "<br>";
-//     cards[i].innerHTML = cards[i].innerHTML + dates[i].charAt(0) + dates[i].charAt(1) + "/" + dates[i].charAt(2) + dates[i].charAt(3);
-// }
-
-
-// var olderVar;
-// var onlyonce = false;
-
-// function sort() {
-//     newNames = [];
-//     newDates = [];
-//     olderVar = undefined;
-
-//     dates = dates.sort((a,b)=>a - b);
-//     for (let i = 0; i < dates.length; i++) {
-//         if ((date.getMonth() + 1 >= Number(dates[0].charAt(0) + dates[0].charAt(1)) && date.getDate() > Number(dates[0].charAt(2) + dates[0].charAt(3))) || date.getMonth() + 1 > Number(dates[0].charAt(0) + dates[0].charAt(1))) {
-//             dates.push(dates.shift());
-//         }
-//     }
-
-//     for (let i = 0; i < names.length; i++) {
-//         newDates.push(dates[i])
-//         evenNewerDates.push(dates[i]);
-//     }
-
-
-//     var temp = evenNewerDates.length
-//     for (let i = 0; i < temp; i++) {
-//         onlyonceplz();
-//         onlyonce = true;
-//         olderVar = oldDates.indexOf(evenNewerDates[0]);
-//         evenNewerDates.splice(0, 1);
-//         if (oldDates.indexOf(evenNewerDates[0]) != olderVar) {
-//             newNames.push(names[oldDates.indexOf(evenNewerDates[0])]);
-//         } else if (oldDates.indexOf(evenNewerDates[0]) == olderVar) {
-//             newNames.push(names[oldDates.indexOf(evenNewerDates[0]) + 1]);
-//         }
-//     }
-
-//     function onlyonceplz() {
-//         if (onlyonce == false) {
-//             if (oldDates.indexOf(evenNewerDates[0]) != olderVar) {
-//                 newNames.push(names[oldDates.indexOf(evenNewerDates[0])]);
-//             } else if (oldDates.indexOf(evenNewerDates[0]) == olderVar) {
-//                 newNames.push(names[oldDates.indexOf(evenNewerDates[0]) + 1]);
-//             }
-//         }
-//     }
-// }
-// sort();
-
-// for (let i = 0; i < names.length; i++) {
-//     cards[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length) + 0];
-//     cards[i].innerHTML = newNames[i] + "<br>";
-//     cards[i].innerHTML = cards[i].innerHTML + newDates[i].charAt(0) + newDates[i].charAt(1) + "/" + newDates[i].charAt(2) + newDates[i].charAt(3);
-// }
-
-// var enableBirthdays = false;
-// var birthdaynumber = 0;
-// var cardnumber = 0;
-// var row;
-// var card;
-// var birthdaylist = []
-
 let interval;
 
 const duration = 15 * 1000,
@@ -110,77 +8,7 @@ function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-// onlyonce = false;
-// function check() {
-//     for (let i = 0; i < names.length; i++) {
-//         if (Number(dates[i].charAt(0).toString() + (dates[i].charAt(1).toString())) === Number(date.getMonth()) + 1) {
-//             if (Number(dates[i].charAt(2).toString() + (dates[i].charAt(3).toString())) === Number(date.getDate())) {
-//                 birthdaypeople.style.display = "flex";
-//                 people.appendChild(cards[i]);
-//                 birthdaylist.push(cards[i]);
-//                 enableBirthdays = true;
-//                 birthdaynumber++;
-//             } else {
-//                 if (birthdaylist.includes(cards[i])) {
-//                     for(let u=0; u<birthdaylist.length; u++) {
-//                         document.getElementById("row" + rows.length).append(birthdaylist.shift());
-//                         birthdaylist.splice(0, 1);
-//                         birthdaynumber--;
-//                     }
-//                 }
-//             }
-//         } else {
-//             if (birthdaylist.includes(cards[i])) {
-//                 for(let u=0; u<birthdaylist.length; u++) {
-//                     document.getElementById("row" + rows.length).append(birthdaylist.shift());
-//                     birthdaylist.splice(0, 1);
-//                     birthdaynumber--;
-//                 }
-//             }
-//         }
-//         if (birthdaynumber == 0) {
-//             enableBirthdays = false;
-//             birthdaypeople.style.display = "none";
-//         }
-//     }
-//     if (enableBirthdays == true) {
-//         for (let x = 1; x < rows.length; x++) {
-//             cardnumber = cardnumber + birthdaynumber;
-//             row = document.getElementById("row" + x);
-//             while (row.childElementCount < 4 && cardnumber<cards.length) {
-//                 card = cards[cardnumber]
-//                 row.append(card);
-//                 cardnumber++;
-//             }
 
-//         }
-//         //activate confetti
-
-//         sort();
-//         for (let i = 0; i < names.length; i++) {
-//             cards[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length) + 0];
-//             cards[i].innerHTML = newNames[i] + "<br>";
-//             cards[i].innerHTML = cards[i].innerHTML + newDates[i].charAt(0) + newDates[i].charAt(1) + "/" + newDates[i].charAt(2) + newDates[i].charAt(3);
-//         }
-//     }
-// }
-
-// var oldVar;
-// var onlyonceagain = false;
-
-// setInterval(function() {
-//     oldVar = date.getDate();
-//     date = new Date();
-//     currentDate.innerHTML = date;
-//     checkTime();
-//     if (onlyonceagain == false) {
-//         onlyonceagain = true;
-//         check();
-//     }
-//     if (oldVar != date.getDate()) {
-//         check();
-//     }
-// }, 100)
 const currentDate = document.getElementById("currentDate");
 const lightColors = ["lightcoral", "lightsalmon", "lightyellow", "lightgreen", "lightblue", "pink"]
 const darkColors = ["maroon", "#BE5504", "#BA8E23", "darkgreen", "darkblue", "purple"];
@@ -213,7 +41,9 @@ async function waitForFetch() {
         sort();
     }
 }
+
 waitForFetch();
+
 var dayInterval = setInterval(function () {
     let pastDate = date;
     updateDate();
@@ -223,17 +53,21 @@ var dayInterval = setInterval(function () {
 }, 100);
 
 function sort() {
-    console.log(data)
     mainContainer.innerHTML = "";
     const entries = Object.entries(data);
 
     entries.sort((a, b) => { return Number(a[1].date) - Number(b[1].date) });
-
-    const currentDateText = Number(String(date.getMonth() + 1) + String(date.getDate()))
+    let currentDateText = Number(String(date.getMonth() + 1) + String(date.getDate()))
     const pastBirthdays = [];
     birthdayList = [];
+
+    if(String(currentDateText).length == 2) {
+        currentDateText = Number(String(currentDateText)[0] + "0" + String(currentDateText)[1]);
+    }
+
     for (let i = 0; i < entries.length; i++) {
         if (Number(entries[i][1].date) < currentDateText) {
+            console.log(entries[i]);
             pastBirthdays.push(entries[i]);
             entries.shift()
             i--;
@@ -313,6 +147,5 @@ function handleBirthdays() {
             card.style.backgroundColor = darkColors[Math.floor(Math.random() * darkColors.length) + 0];
         }
         birthdaysElem.appendChild(card)
-        console.log('sdf')
     })
 }
