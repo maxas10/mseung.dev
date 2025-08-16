dfa0a5d4b03fc1a463559475c0094f4ac6199214
Max <72516602+maxas10@users.noreply.github.com>
2025-08-16 00:12:43 -0700

    add remembering

--- src/app/components/ai.tsx
+++ src/app/components/ai.tsx
@@ -6,23 +6,34 @@ import { useState } from 'react';
 export default function Gemini() {
     const ai = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_API_KEY });
     const [prompt, setPrompt] = useState<string>("");
-    const [response, setResponse] = useState<Array<string>>([]);
+    const [response, setResponse] = useState<string>("");
+    const [remember, setRemember] = useState<boolean>(true);
 
     const main = async () => {
-        const response = await ai.models.generateContent({
+        const geminiResponse = await ai.models.generateContent({
             model: "gemini-2.5-flash",
             contents: prompt,
         });
-        setResponse((response.text ?? "").replace(/\*\*/g, "").replace(/\#\#/g, "").split(/(?=\d+\.)/));
+        setResponse(geminiResponse.text ?? "");
+    };
+
+    const handlePromptChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if(remember) {
+            setPrompt(prev => prev + " | " + e.target.value)
+        } else {
+            setPrompt(e.target.value);
+        }
     };
 
     return <div>
-        <textarea name="form" className="w-full bg-white text-black border-2 placeholder:text-gray-400" onInput={(e) => setPrompt((e.target as HTMLInputElement).value)} placeholder="Prompt" ></textarea>
-        <button onClick={()=>{setResponse(["Loading..."]); main(); }} className="hover:text-gray-400 cursor-pointer">Send</button>
+        <textarea name="form" className="w-full bg-white text-black border-2 placeholder:text-gray-400" onInput={(e) => handlePromptChange(e as any)} placeholder="Prompt" ></textarea>
+        <div className="flex gap-3">
+            <input type="checkbox" onChange={() => {setRemember(!remember)}} checked={remember}/>
+            <p>Remember past prompts</p>
+        </div>
+        <button onClick={() => { setResponse("Loading..."); main(); }} className="hover:text-gray-400 cursor-pointer">Send</button>
         <br />
-        {response.map((item, index) => {
-            return <p key={index} className="ml-10">{item}</p>
-        })}
+        {response}
     </div>
 
 }
