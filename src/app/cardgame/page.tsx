01d08c3a6eb066319e49697a48261bcea932f2b1
Max <72516602+maxas10@users.noreply.github.com>
2025-08-25 23:35:42 -0700

    cardgame v2

--- src/app/cardgame/page.tsx
+++ src/app/cardgame/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import Card from './card';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { use, useEffect, useRef, useState } from 'react';
 
 export default function Page() {
     const [cardsHidden, setCardsHidden] = useState<boolean>(false);
     const [cards, setCards] = useState<string[]>([]);
     const possibleCards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    const inputsRef = useRef<Array<HTMLInputElement | null>>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const formRef = useRef<HTMLFormElement | null>(null);
-    const [start,setStart] = useState<boolean>(false);
+    const [start, setStart] = useState<boolean>(false);
+    const [scoreElem, setScore] = useState<number>(0);
+    const [done, setDone] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
     useEffect(() => {
         const cardsArray = [];
@@ -25,32 +27,34 @@ export default function Page() {
 
     }, []);
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
-        if (e.key === "Backspace" && !e.currentTarget.value && index > 0) {
-            inputsRef.current[index - 1]?.focus();
+    useEffect(() => {
+        if (cardsHidden) {
+            // wait for render flush
+            requestAnimationFrame(() => {
+                inputRef.current?.focus();
+                inputRef.current?.select();
+            });
         }
+    }, [cardsHidden]);
 
-        if (e.key === "Enter") {
-            e.preventDefault();
-            if (index < inputsRef.current.length - 1) {
-                inputsRef.current[index + 1]?.focus();
-            } else {
-                console.log("All inputs filled, submit here!");
-            }
-        }
-    };
+
 
     const handleSubmitGuess = async () => {
-        const guess = inputsRef.current.map(input => (input?.value || '').toUpperCase().trim());
-        let score = 0; // need this one
-        for (let i = 0; i < guess.length; i++) {
-            if (guess[i] == cards[i]) {
+
+        const guessArr = inputRef.current?.value.split(" ") ?? [];
+        let score = 0;
+
+        for (let i = 0; i < guessArr.length; i++) {
+            if (guessArr[i] === cards[i]) {
                 score++;
             }
         }
 
+        setScore(score);
+        setDone(true);
+
         const body = new URLSearchParams();
-        body.append('guesses', guess.join(' ')); // one field
+        body.append('guesses', guessArr.join(' ')); // one field
         body.append(`answer`, cards.join(" "));
         body.append("score", score.toString());
         setLoading(true);
@@ -82,6 +86,8 @@ export default function Page() {
             console.log(error);
             alert(`Request failed: ${error}`);
         }
+
+        console.log(guessArr, cards, score);
     }
 
 
@@ -89,40 +95,30 @@ export default function Page() {
         <div className="absolute w-screen h-screen top-0 left-0 bg-white text-black flex justify-center items-center">
             <button onClick={() => {
                 setStart(true);
+
                 setTimeout(() => {
                     setCardsHidden(true);
                 }, 15000);
 
                 setTimeout(() => {
                     handleSubmitGuess();
-                }, 30000);
+                }, 60000);
             }} className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded cursor-pointer">Start</button>
         </div>
         <img src="loading.gif" className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10 z-10" style={{ display: loading ? "block" : "none" }}></img>
 
-        <div className="absolute w-screen h-screen top-0 left-0 bg-white text-black flex justify-center items-center  gap-4" style={{ display: start ? "flex" : "none" }}>
+        <div className="absolute w-screen h-screen top-0 left-0 bg-white text-black flex justify-center items-center  gap-4" style={{ display: start && !done ? "flex" : "none" }}>
             {cards.map((card, i) => <Card key={i} value={card} hidden={cardsHidden} />)}
             <div className={`${cardsHidden ? '' : 'hidden'}`}>
-                <p>Enter the cards in order (Enter + Backspace to move)</p>
+                <p>Enter the cards in order (Seperate values with a space)</p>
                 {/* <input type="input" className=' border-2'></input> */}
                 <form className="flex gap-2" onSubmit={handleSubmitGuess} ref={formRef}>
-                    {Array.from({ length: 10 }).map((_, i) => (
-                        <input
-                            name={`input-${i}`}
-                            key={i}
-                            ref={(el) => {
-                                inputsRef.current[i] = el;
-                            }}
-                            type="text"
-                            onKeyDown={(e) => handleKeyDown(e, i)}
-                            className="w-16 h-10 border text-center text-lg rounded uppercase"
-                            autoFocus={i == 0 ? true : false}
-                        />
-                    ))}
-
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded cursor-pointer" type="submit" >Submit</button>
+                    <input type="text" name='guess' ref={inputRef} className='border-2 w-full h-15 text-2xl pl-5 uppercase' />
                 </form>
             </div>
+        </div>
+        <div className="absolute w-screen h-screen top-0 left-0 bg-white text-black flex justify-center items-center" style={{ display: done ? "flex" : "none" }}>
+            {!loading && (<p>Score: {scoreElem}</p>)}
         </div>
     </>
 
