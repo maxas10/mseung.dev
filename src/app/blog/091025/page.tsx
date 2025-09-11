6b9eae7de61f543fa983964396c50c541b858d4f
Max <72516602+maxas10@users.noreply.github.com>
2025-09-10 23:16:36 -0700

    more fixes

--- src/app/blog/091025/page.tsx
+++ src/app/blog/091025/page.tsx
@@ -3,7 +3,7 @@ import Title from "../../components/title"
 export default function Page() {
 
     return <>
-        <Title page="Blog" pagesrc="/blog/" page2="#2: Charlie Kirk"></Title>
+        <Title page="Blog" pagesrc="/blog/" page2="#2: Charlie Kirk 9/10/25"></Title>
         <p className="indent-8">This guy just got killed today. I wasn&apos;t going to write something today until I saw the news.</p>
         <br />
         <p className="indent-8">I remember seeing Kirk arguing with some college students a few weeks ago. Some random account on Instagram made fun of the way his face looks. I am not into politics, but killing someone over this is just crazy. What happened to free speech?</p>
